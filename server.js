f3149736786cc104e6166c8c74b82544407ad21d
hariesh-sambath <shariesh2002@gmail.com>
2023-01-09 12:52:52 +0400

    added cors

--- server.js
+++ server.js
@@ -3,19 +3,28 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const session = require("express-session");
+const cors = require("cors");
+// const session = require("express-session");
 const cookie_parser = require("cookie-parser");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(
-   session({
-      secret: process.env.SESSION_SECRET,
-      saveUninitialized: true,
-      cookie: { maxAge: 1000 * 60 * 1 }, // 1 minute in milliseconds
-      resave: false,
+   cors({
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
    })
 );
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(
+//    session({
+//       secret: process.env.SESSION_SECRET,
+//       saveUninitialized: true,
+//       cookie: { maxAge: 1000 * 60 * 1 }, // 1 minute in milliseconds
+//       resave: false,
+//    })
+// );
 app.use(cookie_parser());
 
 const connectDB = require("./dbConfig");
@@ -24,7 +33,7 @@ connectDB();
 // routes
 app.use("/api/authenticate", require("./routers/authenticate"));
 app.use("/api/register", require("./routers/register"));
-app.use("/api/edu-supplies", require("./routers/edu-supplies"))
+app.use("/api/edu-supplies", require("./routers/edu-supplies"));
 
 mongoose.connection.once("open", () => {
    console.log("database connected!");
